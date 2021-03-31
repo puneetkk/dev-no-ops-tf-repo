eb0d30165fcc63a2cdf468cf0a5126d2ffb2fb19
puneetkk <puneet.kankane@gmail.com>
2021-03-31 22:12:57 +0100

    Removed duplicate check

--- modules/aad/main.tf
+++ modules/aad/main.tf
@@ -16,11 +16,9 @@ resource "azuread_group" "DEV-USER-GROUP" {
   display_name            = "User Group"
   description             = "Dvelopment User Group"
   owners                  = [var.group_owner]
-  prevent_duplicate_names = true
 }
 
 resource "azuread_group" "DEV-ADMIN-GROUP" {
   display_name            = "Admin Group"
   description             = "Dvelopment Admin Group"
-  prevent_duplicate_names = true
 }
