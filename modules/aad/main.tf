b2b0e61afef89b44e78b7f85a7de3288eb7682cd
puneetkk <puneet.kankane@gmail.com>
2021-03-31 23:03:22 +0100

    Added application

--- modules/aad/main.tf
+++ modules/aad/main.tf
@@ -21,4 +21,26 @@ resource "azuread_group" "DEV-USER-GROUP" {
 resource "azuread_group" "DEV-ADMIN-GROUP" {
   display_name            = "Admin Group"
   description             = "Dvelopment Admin Group"
+  owners                  = [var.group_owner]
+}
+
+resource "azuread_application" "myTFDevSPAFrontEnd" {
+    display_name               = "myTFDevSPAFrontEnd"
+    homepage                   = "https://homepage"
+    identifier_uris            = ["https://uri"]
+    reply_urls                 = ["https://replyurl"]
+    available_to_other_tenants = false
+    oauth2_allow_implicit_flow = false
+    owners                     = [var.group_owner]
+
+
+}
+
+resource "azuread_application_app_role" "AdminRole" {
+  application_object_id = azuread_application.myTFDevSPAFrontEnd.id
+  allowed_member_types  = ["User"]
+  description           = "Admins can manage roles and perform all task actions"
+  display_name          = "Admin"
+  is_enabled            = true
+  value                 = "administer"
 }
